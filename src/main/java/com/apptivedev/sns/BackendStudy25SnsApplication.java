b3a25493b281a8c5a8b5d58ae2b58769c5f62d8f
Jesuji <jesuji030502@naver.com>
2026-09-18 17:02:47 +0900

    chore: initialize SpringBoot study project

---
+++ src/main/java/com/apptivedev/sns/BackendStudy25SnsApplication.java
@@ -0,0 +1,13 @@
+package com.apptivedev.sns;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendStudy25SnsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendStudy25SnsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/apptivedev/sns/BackendStudy25SnsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apptivedev.sns;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendStudy25SnsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
